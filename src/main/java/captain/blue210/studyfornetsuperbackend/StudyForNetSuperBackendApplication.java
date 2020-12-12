ca6088f1d0fb41b4e63ff85e07ad236cf12b1623
captain-blue210 <captain.blue210@gmail.com>
2020-12-12 09:19:06 +0900

    chore:create Spring Boot Projects.

---
+++ src/main/java/captain/blue210/studyfornetsuperbackend/StudyForNetSuperBackendApplication.java
@@ -0,0 +1,13 @@
+package captain.blue210.studyfornetsuperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyForNetSuperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyForNetSuperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/captain/blue210/studyfornetsuperbackend/StudyForNetSuperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package captain.blue210.studyfornetsuperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyForNetSuperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
